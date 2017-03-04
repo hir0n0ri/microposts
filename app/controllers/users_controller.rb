26879ef6054b2337262b64e35933b71e9a1bffb3
Hiro Ota <hir0n0ri.0ta@gmail.com>
2017-03-04 03:20:04 +0000

    lesson7_task1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
   
   def show # 追加
    @user = User.find(params[:id])
@@ -17,11 +18,30 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+  
+  def edit
+  end
+  
+  def update
+    if @user.update(user_params)
+    redirect_to root_path , notice: '編集しました'
+  else
+    render 'edit'
+  end
+end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  def set_user
+  @user = User.find(params[:id])
+  unless current_user = @user
+  redirect_to root_path 
   end
 end
+end
+
+ 
