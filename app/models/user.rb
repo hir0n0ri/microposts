f4f461092aace231109ac576b51a8a7b87a75ec5
Hiro Ota <hir0n0ri.0ta@gmail.com>
2017-03-10 13:44:38 +0000

    modiied

--- app/models/user.rb
+++ app/models/user.rb
@@ -37,8 +37,7 @@ class User < ActiveRecord::Base
     following_users.include?(other_user)
   end
   
-   def feed_items
+  def feed_items
     Micropost.where(user_id: following_user_ids + [self.id])
- 　 end
-  
+  end
 end
